3f5f06cbcb7912ebac6b1116712fe065e22c59a2
sfouziya123 <116749596+sfouziya123@users.noreply.github.com>
2023-04-11 22:29:50 +0530

    Add files via upload
    k

---
+++ week_01/day_4/evenlengthwords.java
@@ -0,0 +1,28 @@
+//Write a program to print all even length words present in the sentence?
+//Example : Input  :  String str = “JTD is running JTBB Bootcamp”                      
+ //Output : is  JTBB  Bootcamp
+ 
+import java.util.Scanner;
+class temp 
+{
+    public static void main(String[] args)
+    {
+        String str = "Hai nandana gopala hare";
+        int start = 0;
+        int prev = 0;
+        while(start < str.length())
+        {
+            while(start < str.length() && str.charAt(start) != ' ')
+            {
+                start++;
+            }
+            String temp = str.substring(prev,start);
+            if(temp.length() % 2 == 0)
+            {
+                System.out.println(temp);
+            }
+            start = start+1;
+            prev = start;
+        }
+    }
+}
