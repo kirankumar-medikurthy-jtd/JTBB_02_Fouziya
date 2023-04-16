ce487e0c55ae2a7767175b4b347ecd5a764c3b53
sfouziya123 <116749596+sfouziya123@users.noreply.github.com>
2023-04-16 22:55:44 +0530

    Add files via upload
    done

--- week_02/day_4/palindromeodd.java
+++ week_02/day_4/palindromeodd.java
@@ -1,29 +1,30 @@
+/*Write a program to print odd length and longest  palindromic substring from the string (Fouziya)
+            Example: String str = “babad”*/
+
 public class main
 {
 	public static void main(String[] args)
 	{
 		String str = "babad";
-		
+		//largest substring
+		 String LargestSubstring = str.charAt(0) + " " ;
+		 //run the loop
 			for(int j = 0; j < str.length(); j++)
 			{
-				//String substr="";
-				
 				for(int f = j+1; f <= str.length(); f++)
 				{
-					String substr=str.substring(j,f);
-					if(findPalindrom(substr) && substr.length()%2==1)
+					String substr=str.substring(j,f);//print the i,j
+					//call the function and check the odd or even length and also the largestsubstring length
+					if(findPalindrom(substr) && substr.length()%2==1 && substr.length() >= LargestSubstring.length())
 					{
 						System.out.println(substr);
+						LargestSubstring = substr;
 					}
 				}
-				
-			
 			}
 		
-		
-		
 	}
-	
+	//check palindrome or not
 	public static boolean findPalindrom(String str)
 	{
 		int start = 0;
